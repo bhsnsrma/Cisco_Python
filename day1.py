cc352cecfb8395363521857afdbce12b3fec23f6
Bhushan <bhshsrma@hotmail.com>
2019-09-23 13:18:24 +0530

    prelunch

---
+++ day1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+print("Hello World")
+x=3
+y="ok google"
+z=[4,22,x,y]
+
+print(y)
+print(z)
+
+print(x,y)
+
---
+++ timedelay.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import time
+
+print("hello world")
+time.sleep(3)
+print("cisco automation")
+time.sleep(5)
+print("current time is",time.ctime())
+
